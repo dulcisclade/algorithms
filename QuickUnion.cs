1423ce5ace3e2024f6bf700449a05df4f680c1e0
dulcis_clade <31682990+dulcisclade@users.noreply.github.com>
2021-10-28 10:44:35 +0300

    Added Quick Union Algorithm implemented on c#

---
+++ QuickUnion.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace concole
+{
+    public class QuickUnion
+    {
+        private Point[] points;
+
+        public QuickUnion(int count)
+        {
+            points = new Point[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                points[i] = new Point(i);
+            }
+        }
+    }
+
+    public class Point
+    {
+        public int Value { get; private set; }
+        public Point NextPoint { get; private set; }
+        public int Size { get; private set; }
+
+        public Point(int value)
+        {
+            this.Value = value;
+            this.NextPoint = this;
+            Size = 1;
+        }
+
+        private Point Root()
+        {
+            Point currentPoint = this;
+            while (currentPoint != currentPoint.NextPoint)
+            {
+                currentPoint = NextPoint.NextPoint;
+            }
+
+            return currentPoint;
+        }
+
+        public bool IsConnectedWith(Point item)
+        {
+            var thisRoot = this.Root();
+            var itemRoot = item.Root();
+
+            return IsRootsEquals(thisRoot, itemRoot);
+        }
+
+        private bool IsRootsEquals(Point thisRoot, Point itemRoot)
+        {
+            return thisRoot.Value == itemRoot.Value && thisRoot.Size == itemRoot.Size;
+        }
+
+
+        public void Connect(Point item)
+        {
+            var thisRoot = this.Root();
+            var itemRoot = item.Root();
+
+            if (IsRootsEquals(thisRoot, itemRoot))
+            {
+                return;
+            }
+
+            if (itemRoot.Size >= thisRoot.Size)
+            {
+                itemRoot.Size += thisRoot.Size;
+                thisRoot.Size = 1;
+                thisRoot.NextPoint = itemRoot;
+            }
+
+            else
+            {
+                thisRoot.Size += itemRoot.Size;
+                itemRoot.Size = 1;
+                itemRoot.NextPoint = itemRoot;
+            }
+        }
+    }
+}
