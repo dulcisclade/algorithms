ac7742769ae674cecd1851a4beeff95d72bde17e
Darya Liashkevich <diline2817@gmail.com>
2022-04-06 23:49:06 +0400

    Added ThreeSum

--- Sum.cs
+++ Sum.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
-
-namespace ConsoleApp2
+namespace concole
 {
     public class Program
     {
@@ -13,7 +12,7 @@ namespace ConsoleApp2
                 {
                     if (nums[i] * 2 == target)
                     {
-                        return new[] {dict[nums[i]], i};
+                        return new[] { dict[nums[i]], i };
                     }
                 }
                 else
@@ -26,11 +25,61 @@ namespace ConsoleApp2
             {
                 if (dict.ContainsKey(nums[i]) && i != dict[nums[i]])
                 {
-                    return new[] {i, dict[nums[i]]};
+                    return new[] { i, dict[nums[i]] };
                 }
             }
 
             return null;
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> results = new List<IList<int>>();
+
+            int? prev = null;
+            for (int k = nums.Length - 1; k >= 2; k--)
+            {
+                if (nums[k] == prev)
+                {
+                    continue;
+                }
+
+                int i = 0;
+                int j = k - 1;
+                while (i < j)
+                {
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                    {
+                        results.Add(new[] { nums[k], nums[i], nums[j] });
+                        prev = nums[k];
+                        while (i + 1 < j && nums[i] == nums[i + 1])
+                        {
+                            i++;
+                        }
+
+                        while (j - 1 > i && nums[j] == nums[j - 1])
+                        {
+                            j--;
+                        }
+
+                        i++;
+                        j--;
+                    }
+
+                    else if (nums[i] + nums[j] + nums[k] < 0)
+                    {
+                        i++;
+                    }
+
+                    else if (nums[i] + nums[j] + nums[k] > 0)
+                    {
+                        j--;
+                    }
+                }
+            }
+
+            return results;
         }
     }
 }
