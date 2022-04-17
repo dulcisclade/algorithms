979a3546909f6befb9c71084482461ab1f1c7142
Darya Liashkevich <diline2817@gmail.com>
2022-04-17 14:25:28 +0400

    Added FourSum

--- Sum.cs
+++ Sum.cs
@@ -32,13 +32,13 @@ namespace concole
             return null;
         }
 
-        public static IList<IList<int>> ThreeSum(int[] nums)
+        public IList<IList<int>> ThreeSum(int[] nums)
         {
             Array.Sort(nums);
             IList<IList<int>> results = new List<IList<int>>();
 
             int? prev = null;
-            for (int k = nums.Length - 1; k >= 2; k--)
+            for (int k = nums.Length - 1; k >= 2; k--) //max
             {
                 if (nums[k] == prev)
                 {
@@ -75,6 +75,67 @@ namespace concole
                     else if (nums[i] + nums[j] + nums[k] > 0)
                     {
                         j--;
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            IList<IList<int>> results = new List<IList<int>>();
+            if (nums.Length < 3)
+            {
+                return results;
+            }
+
+            Array.Sort(nums);
+
+            for (int k = nums.Length - 1; k >= 3; k--) //max
+            {
+                if (k != nums.Length - 1 && nums[k] == nums[k + 1])
+                {
+                    continue;
+                }
+
+                for (int l = k - 1; l >= 2; l--) //premax
+                {
+                    if (l != k - 1 && nums[l] == nums[l + 1])
+                    {
+                        continue;
+                    }
+
+                    int j = l - 1;
+                    int i = 0;
+                    while (i < j && i < l)
+                    {
+                        if (nums[k] + nums[l] + nums[j] + nums[i] == target)
+                        {
+                            results.Add(new[] {nums[k], nums[l], nums[j], nums[i]});
+                            while (i < j && nums[i] == nums[i + 1])
+                            {
+                                i++;
+                            }
+
+                            while (j > i && nums[j] == nums[j - 1])
+                            {
+                                j--;
+                            }
+
+                            i++;
+                            j--;
+                        }
+
+                        else if (nums[k] + nums[l] + nums[j] + nums[i] < target)
+                        {
+                            i++;
+                        }
+
+                        else if (nums[k] + nums[l] + nums[j] + nums[i] > target)
+                        {
+                            j--;
+                        }
                     }
                 }
             }
